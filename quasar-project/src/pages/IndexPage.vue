b1c34883680a253e66a59b329148dce2385d0637
Aleksey <rusal@bk.ru>
2022-03-24 11:39:35 +0300

    init

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="flex flex-center">
+ <div class="q-pa-md row justify-center">
+
+    <q-input standout v-model="text" :dense="dense" />
+    {{text}}
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        :text="['hey, how are you?']"
+        stamp="7 minutes ago"
+        sent
+        bg-color="amber-7"
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+        :text="[
+          'doing fine, how r you?',
+          'I just feel like typing a really, really, REALLY long message to annoy you...'
+        ]"
+        size="6"
+        stamp="4 minutes ago"
+        text-color="white"
+        bg-color="primary"
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+        :text="['Did it work?']"
+        stamp="1 minutes ago"
+        size="8"
+        text-color="white"
+        bg-color="primary"
+      />
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup(){
+    return { text: ref(""),}
+  }
+})
+</script>
